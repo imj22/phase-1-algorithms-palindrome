dfb1c1b4044a83a7803439e71613bc6a89159af1
imj22 <98693440+imj22@users.noreply.github.com>
2022-04-03 13:42:04 +0200

    commit tree e2b9d0a5c795d05b38462ac4a54799c5c95e14e2

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let lettersArr = Array.from(word)
+  // console.log(lettersArr)
+  let reversedArr = lettersArr.reverse()
+  // console.log(reversedArr)
+  let reversedWord = reversedArr.join("")
+  // console.log(reversedWord)
+  // if (word === reversedWord) {return true} else {return false}
+  // above is alternate version to lines 10 & 11. proof can be done with tenerary expression OR with if else statement
+  let test = word === reversedWord ? true : false;
+  return test
 }
 
-/* 
-  Add your pseudocode here
-*/
 
-/*
-  Add written explanation of your solution here
-*/
+// deliverable: write a function that takes in a string as an argument and returns true (boolean) for a words that are the same backwards and forwards, including singular letter words like "a" and returns false for any words that do not meet this condition. 
+
+// pseudocode: 
+// turn string into reverse, and set it equal to a variable (placeholder backwards)
+// write if else statement or tenerary expression, with condition if word parameter === backwards, return true. else return false
+// write a function of if else ? that clears out the conditions of the arguments accepted. 
+// write a tenerary statement ? 
+  // *function* ? return true : return false 
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
